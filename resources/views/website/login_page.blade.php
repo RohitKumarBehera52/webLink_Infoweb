d697482fd20c6f296bc95a1f20aaaa7492e28ce5
vivek jha <vivek.jha0151@gmail.com>
2020-12-21 21:34:45 +0530

    hello

--- resources/views/website/login_page.blade.php
+++ resources/views/website/login_page.blade.php
@@ -6,7 +6,7 @@
 	<h1 style="color: purple;"><b>Join our happy family</b></h1>
 	</div>
 <div class="row h-100 justify-content-center align-items-center">
-	<h1 style="color: purple;"><b>Register free.</b></h1>
+	<h1 style="color: purple;"><b>Register Free.</b></h1>
 	</div>
 
 @endsection
